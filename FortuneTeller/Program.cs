8dd884cbcd773ca26ffa3e6f3d054244e3eca56c
Jen E <jenevans3ten@gmail.com>
2020-05-19 10:40:26 -0400

    added instructions for user

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -6,7 +6,9 @@ namespace FortuneTeller
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello! Welcome to Fortune Teller");
+            Console.WriteLine("Hello! Welcome to Fortune Teller.");
+            Console.WriteLine("I am going to tell your fortune.");
+
         }
     }
 }
